56d914cff379c0fd1aca1f7f80177801f2d6a5b3
Patchnida <patchnida2547@gmail.com>
2026-03-31 15:18:13 +0700

    adding super user

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# สร้าง Superuser แบบอัตโนมัติ (จะข้ามถ้ามี user นี้อยู่แล้ว)
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+  python manage.py createsuperuser \
+    --no-input \
+    --username $DJANGO_SUPERUSER_USERNAME \
+    --email $DJANGO_SUPERUSER_EMAIL
+fi
